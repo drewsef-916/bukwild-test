3bc65cfb6b03010057b061316567a488e4af1612
drewsef-916 <drewm541@gmail.com>
2020-06-01 08:39:30 -0700

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,78 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div id="app" :class="currentMarqueeBackground">
+    <header>
+      <section>
+        <img src="@/assets/abc_logo.svg"/>
+        <nav>
+          <a :click="updateMarqueeBody('Industries')">Industries</a>
+          <a :click="updateMarqueeBody('Services')">Services</a>
+          <a :click="updateMarqueeBody('About Us')">About Us</a>
+        </nav>
+      </section>
+      <button>
+        Contact Us
+      </button>
+    </header>
+    <MarqueeBody v-bind="currentMarqueeBody"></MarqueeBody>
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import MarqueeBody from "./components/MarqueeBody.vue";
+
+import { pages } from "@/assets/content.json";
 
 export default {
-  name: "App",
+  // name: "App",
   components: {
-    HelloWorld
+    MarqueeBody 
+  },
+  data() {
+    return {
+      marqueeList: pages,
+      currentMarqueeBody: pages[0]
+    }
+  },
+  methods: {
+    updateMarqueeBody: function(title) {
+      // console.log(this.marqueeList);
+      console.log(title);
+      const correctBody = this.marqueeList.forEach(item => console.log(item.title));
+      console.log(correctBody);
+    },
+  },
+  mounted() {
+    // console.log(this.currentMarqueeBody);
+  },
+  computed: {
+    currentMarqueeBackground: function() {
+      return this.currentMarqueeBody.title.toLowerCase();
+    } 
   }
 };
 </script>
 
 <style lang="scss">
+body {
+  margin: 0;
+}
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  // -webkit-font-smoothing: antialiased;
+  // -moz-osx-font-smoothing: grayscale;
+  font-family: Helvetica, Arial, sans-serif;
+  min-height: 100vh;
+  color: #fff;
+  padding: 10px;
+  background-size: cover;
+
+  &.industries {
+    background: url("./assets/slide_one.jpg") no-repeat center center;
+  }
+  &.services {
+    background: url("./assets/slide_two.jpg") no-repeat center center;
+  }
+  &.contact-us {
+    background: url("./assets/slide_three.jpg") no-repeat center center;
+  }
 }
 </style>
---
+++ src/components/MarqueeBody.vue
@@ -0,0 +1,33 @@
+<template>
+  <main>
+    <section>
+      <div class="headline">
+        <span>{{title}}</span>
+      </div>
+      <div class="subheadline">
+      </div>
+    </section>
+  </main>
+</template>
+
+<script>
+
+export default {
+  props: ["title", "slug", "blocks"],
+  data: function() {
+    return {
+      type: this.blocks[0].type,
+      headline: this.blocks[0].headline,
+      subhead: this.blocks[0].subhead,
+      cta: this.blocks[0].cta,
+      background: this.blocks[0].background
+    };
+  },
+};
+</script>
+
+<style lang="scss">
+  .headline, .subheadline {
+    color: #333
+  }
+</style>
